f839ffde29d1f2d68f06d9b6f221d13720e7e83d
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-02-10 17:06:49 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- Command/VhostDefineCommand.php
+++ Command/VhostDefineCommand.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class VhostDefineCommand extends ContainerAwareCommand
 {
