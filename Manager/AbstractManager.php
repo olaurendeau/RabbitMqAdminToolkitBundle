04fb1b74c6d9353fab79d1d53d643f4fa6897882
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-02-11 18:17:02 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- Manager/AbstractManager.php
+++ Manager/AbstractManager.php
@@ -41,17 +41,17 @@ abstract class AbstractManager
     }
 
     private function array_diff_assoc_recursive($array1, $array2) {
-        $difference=array();
-        foreach($array1 as $key => $value) {
-            if( is_array($value) ) {
-                if( !isset($array2[$key]) || !is_array($array2[$key]) ) {
+        $difference = array();
+        foreach ($array1 as $key => $value) {
+            if (is_array($value)) {
+                if (!isset($array2[$key]) || !is_array($array2[$key])) {
                     $difference[$key] = $value;
                 } else {
                     $new_diff = $this->array_diff_assoc_recursive($value, $array2[$key]);
-                    if( !empty($new_diff) )
+                    if (!empty($new_diff))
                         $difference[$key] = $new_diff;
                 }
-            } else if( !array_key_exists($key,$array2) || $array2[$key] !== $value ) {
+            } else if (!array_key_exists($key, $array2) || $array2[$key] !== $value) {
                 $difference[$key] = $value;
             }
         }
