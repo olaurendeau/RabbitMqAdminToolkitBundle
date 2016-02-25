ff93a32ddc4fcfb7de2dcd110d5d5e831393dceb
Olivier Laurendeau <olaurendeau@lafourchette.com>
2016-02-25 10:05:59 +0100

    Update AbstractManager.php

--- Manager/AbstractManager.php
+++ Manager/AbstractManager.php
@@ -24,7 +24,7 @@ abstract class AbstractManager
      */
     protected function isUpToDate($configuration, $remoteConfiguration)
     {
-        $diff = $this->array_diff_assoc_recursive($configuration, $remoteConfiguration);
+        $diff = $this->arrayDiffAssocRecursive($configuration, $remoteConfiguration);
 
         foreach ($remoteConfiguration as $k => $v) {
             if (!isset($configuration[$k])) {
@@ -34,22 +34,22 @@ abstract class AbstractManager
 
         $diff = array_merge_recursive(
             $diff,
-            $this->array_diff_assoc_recursive($remoteConfiguration, $configuration)
+            $this->arrayDiffAssocRecursive($remoteConfiguration, $configuration)
         );
 
         return empty($diff);
     }
 
-    private function array_diff_assoc_recursive($array1, $array2) {
+    private function arrayDiffAssocRecursive($array1, $array2) {
         $difference = array();
         foreach ($array1 as $key => $value) {
             if (is_array($value)) {
                 if (!isset($array2[$key]) || !is_array($array2[$key])) {
                     $difference[$key] = $value;
                 } else {
-                    $new_diff = $this->array_diff_assoc_recursive($value, $array2[$key]);
+                    $new_diff = $this->arrayDiffAssocRecursive($value, $array2[$key]);
                     if (!empty($new_diff)) {
-                                            $difference[$key] = $new_diff;
+                        $difference[$key] = $new_diff;
                     }
                 }
             } else if (!array_key_exists($key, $array2) || $array2[$key] !== $value) {
